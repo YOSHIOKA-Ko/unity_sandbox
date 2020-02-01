65f3497f7875b3fa2c4ffb5a10543c00ed2c193f
YOSHIOKA Ko <madonote.ko57@gmail.com>
2020-02-01 23:53:27 +0900

    Learn use to Class

---
+++ Assets/UnityTextbookFor2019/Class.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    private int hp = 100;
+    private int power = 50;
+
+    public void Attack()
+    {
+        Debug.Log(this.power + " damage to target.");
+    }
+
+    public void Damage(int damage)
+    {
+        this.hp -= damage;
+        Debug.Log("Get " + damage + " damage.");
+    }
+}
+
+public class Class : MonoBehaviour
+{
+    private void Start()
+    {
+        Player myPlayer = new Player();
+        myPlayer.Attack();
+        myPlayer.Damage(30);
+    }
+}
