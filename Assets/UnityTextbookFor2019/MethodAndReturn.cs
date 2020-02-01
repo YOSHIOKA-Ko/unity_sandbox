5f1cd9da7e428a5eb5ed60382a574d68cadf81a9
YOSHIOKA Ko <madonote.ko57@gmail.com>
2020-02-01 19:48:39 +0900

    Learn use to Method and Return

---
+++ Assets/UnityTextbookFor2019/MethodAndReturn.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MethodAndReturn : MonoBehaviour
+{
+    int Add(int a, int b)
+    {
+        int c = a + b;
+        return c;
+    }
+
+    private void Start()
+    {
+        int answer;
+        answer = Add(2, 3);
+        Debug.Log(answer);
+
+        // 変数を引数にあてることもできる
+        int num1 = 20;
+        int num2 = 30;
+        answer = Add(num1, num2);
+        Debug.Log(answer);
+    }
+}
