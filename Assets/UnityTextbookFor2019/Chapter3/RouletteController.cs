d7c8f867e6c8f1a411af56e859948c5b661eb31d
YOSHIOKA Ko <madonote.ko57@gmail.com>
2020-02-03 03:03:24 +0900

    Add roulette controller

---
+++ Assets/UnityTextbookFor2019/Chapter3/RouletteController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    private float rotateSpeed = 0;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.rotateSpeed = 10;
+            Debug.Log("Set speed" + rotateSpeed);
+        }
+
+        transform.Rotate(0, 0, this.rotateSpeed);
+    }
+}
