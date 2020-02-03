f62e3bcb4aab17e9e0d21542460c6310a316196d
YOSHIOKA Ko <madonote.ko57@gmail.com>
2020-02-04 04:02:27 +0900

    Translate the car using swipe

--- Assets/UnityTextbookFor2019/Chapter4/CarController.cs
+++ Assets/UnityTextbookFor2019/Chapter4/CarController.cs
@@ -6,12 +6,23 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
     private float speed = 0;
+    private Vector2 startPosition;
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            this.speed = .2f;
+            this.startPosition = Input.mousePosition;
+            Debug.Log(startPosition);
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPosition = Input.mousePosition;
+            Debug.Log(endPosition);
+            float swipeLength = endPosition.x - this.startPosition.x;
+
+            this.speed = swipeLength / 500.0f;
+            Debug.Log("Swipe length: " + swipeLength + " / First speed: " + speed);
         }
 
         transform.Translate(this.speed, 0, 0);
