6247fb4048be7c1522776469785cb4ed1e0f6726
YOSHIOKA Ko <madonote.ko57@gmail.com>
2020-02-04 03:43:41 +0900

    Translate the car using the event of mouse down

---
+++ Assets/UnityTextbookFor2019/Chapter4/CarController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private float speed = 0;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.speed = .2f;
+        }
+
+        transform.Translate(this.speed, 0, 0);
+        this.speed *= .98f;
+    }
+}
