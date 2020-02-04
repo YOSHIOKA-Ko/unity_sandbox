3be05fac9bcefc3441473858824d5ac48ec59724
YOSHIOKA Ko <madonote.ko57@gmail.com>
2020-02-05 02:31:43 +0900

    Add game director

---
+++ Assets/UnityTextbookFor2019/Chapter4/GameDirector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour
+{
+    [SerializeField] private CarController car;
+    [SerializeField] private SpriteRenderer flag;
+    [SerializeField] private Text distance;
+
+    private void Update()
+    {
+        float length = this.flag.transform.position.x - this.car.transform.position.x;
+        this.distance.GetComponent<Text>().text = length.ToString("F2");
+    }
+}
