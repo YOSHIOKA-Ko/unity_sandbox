c27f92a1ad93abadb449de7991e162cac43a9421
YOSHIOKA Ko <madonote.ko57@gmail.com>
2020-02-06 00:59:51 +0900

    Add player controller

---
+++ Assets/UnityTextbookFor2019/Chapter5/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private PlayerController player;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            player.transform.Translate(-3, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            player.transform.Translate(3, 0, 0);
+        }
+    }
+}
