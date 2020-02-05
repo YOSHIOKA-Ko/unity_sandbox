e18e6d055fdc4ffb89713fa7a7ac3ef5196d6cde
YOSHIOKA Ko <madonote.ko57@gmail.com>
2020-02-06 01:38:34 +0900

    Add arrow controller

---
+++ Assets/UnityTextbookFor2019/Chapter5/ArrowController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour
+{
+    [SerializeField] private ArrowController arrow;
+    [SerializeField] private Camera mainCamera;
+
+    Rect viewPortRect = new Rect(0, 0, 1,1);
+
+    private void Update()
+    {
+        arrow.transform.Translate(0, -.1f, 0);
+
+        var viewPortPosition = mainCamera.WorldToViewportPoint(arrow.transform.position);
+        if (!viewPortRect.Contains(viewPortPosition))
+        {
+            Destroy(arrow.gameObject);
+        }
+    }
+}
