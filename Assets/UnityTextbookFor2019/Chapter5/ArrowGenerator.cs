fc92e56ac0da58a7fc62435b9a852f80903f2939
YOSHIOKA Ko <madonote.ko57@gmail.com>
2020-02-10 05:17:26 +0900

    Add arrow generator

---
+++ Assets/UnityTextbookFor2019/Chapter5/ArrowGenerator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ArrowGenerator : MonoBehaviour
+{
+    [SerializeField] private ArrowController arrowPrefab;
+    private float span = .5f;
+    private float delta = 0;
+
+    private void Update()
+    {
+        this.delta += Time.deltaTime;
+        if (this.delta > this.span)
+        {
+            this.delta = 0;
+            GameObject go = Instantiate(arrowPrefab.gameObject) as GameObject;
+            int px = Random.Range(-6, 7);
+            go.transform.position = new Vector3(px, 7, 0);
+        }
+    }
+}
