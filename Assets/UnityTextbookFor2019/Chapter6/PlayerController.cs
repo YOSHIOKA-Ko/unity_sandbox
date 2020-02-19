1ad4c5df572fe302d96d7d85cd787b1fd7aa7ee6
YOSHIOKA Ko <madonote.ko57@gmail.com>
2020-02-20 02:16:04 +0900

    Add action of jumping that assign to the space key

---
+++ Assets/UnityTextbookFor2019/Chapter6/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace UnityTextbookFor2019.Chapter6
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerController : MonoBehaviour
+    {
+        float jumpForce = 680.0f;
+        [SerializeField] Rigidbody2D rigid2D;
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rigid2D.AddForce(transform.up * jumpForce);
+            }
+        }
+
+        void OnValidate()
+        {
+            rigid2D = GetComponent<Rigidbody2D>();
+        }
+    }
+}
