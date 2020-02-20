36e325e7af12935e4f1f97520ebe1fd4e0b111c1
YOSHIOKA Ko <madonote.ko57@gmail.com>
2020-02-21 03:52:55 +0900

    Add actions moving left and right

--- Assets/UnityTextbookFor2019/Chapter6/PlayerController.cs
+++ Assets/UnityTextbookFor2019/Chapter6/PlayerController.cs
@@ -5,8 +5,11 @@ namespace UnityTextbookFor2019.Chapter6
     [RequireComponent(typeof(Rigidbody2D))]
     public class PlayerController : MonoBehaviour
     {
-        float jumpForce = 680.0f;
         [SerializeField] Rigidbody2D rigid2D;
+        [SerializeField] SpriteRenderer sprite;
+        float jumpForce = 680.0f;
+        float walkForce = 30.0f;
+        float maxWalkSpeed = 2.0f;
 
         void Update()
         {
@@ -14,6 +17,20 @@ namespace UnityTextbookFor2019.Chapter6
             {
                 rigid2D.AddForce(transform.up * jumpForce);
             }
+
+            int key = 0;
+            if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+            if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+            float speedx = Mathf.Abs(rigid2D.velocity.x);
+
+            if (speedx < maxWalkSpeed)
+            {
+                rigid2D.AddForce(transform.right * key * walkForce);
+            }
+
+            if (key == 1) sprite.flipX = false;
+            if (key == -1) sprite.flipX = true;
         }
 
         void OnValidate()
