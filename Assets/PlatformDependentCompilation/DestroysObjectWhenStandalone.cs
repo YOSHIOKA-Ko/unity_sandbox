4f39a3d5e6efd11363c4f8d018a098bee26ed54f
YOSHIOKA Ko <madonote.ko57@gmail.com>
2020-11-30 00:36:21 +0900

    Add script of destroys object when standalone

---
+++ Assets/PlatformDependentCompilation/DestroysObjectWhenStandalone.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace PlatformDependentCompilation
+{
+    public class DestroysObjectWhenStandalone: MonoBehaviour
+    {
+        void Start()
+        {
+#if UNITY_STANDALONE
+            Destroy(this.gameObject);
+            Debug.Log("Destroyed a " + this.gameObject.name + ".");
+#endif
+        }
+    }
+}
